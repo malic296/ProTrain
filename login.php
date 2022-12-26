04856c112736c89f5524233b3ee7301a1ac67133
malic296 <69051590+malic296@users.noreply.github.com>
2022-12-26 11:34:48 +0100

    Add files via upload
    Vytvořil jsem základní styly na login a register... ještě bude potřeba udělat barvy z vybrané palety...

--- login.php
+++ login.php
@@ -5,16 +5,21 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>DB testing</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
     <script src='main.js'></script>
 </head>
 <body>
-    <h2>Login</h2>
-    <form action="app.php" method="post">
-        <input type="text" name="login" placeholder="username"> <br>
-        <input type="password" name="password" placeholder="*********"> <br>
-        <input type="submit" name="subLogin" id="" value="Login">
-        <div class="logPage">Not registered? <a href=" signup.php">Sign Up</a></div>
-    </form>    
+    <div class="login">
+        <h2>Login</h2>
+        <form action="app.php" method="post" class = form_login>
+            <div class="login_uname"><input type="text" name="login" placeholder="username" class = "text"> </div>
+            <div class="login_passwd"><input type="password" name="password" placeholder="*********" class = "text"> </div><br>
+            <div class="login_button"><input type="submit" name="subLogin" id="" value="Login" class = "submit"></div>
+            <div class="login_register">Not registered? <br> <a href="signup.php"><input type="submit" value="Sign Up" class = "submit_small"></a></div>
+        </form>  
+    </div>  
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -5,18 +5,18 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>ProTrain Registration</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
     <script src='main.js'></script>
 </head>
 <body>
-    <div class="box">
-        <form action="app.php" method="post">
-            <h2>Register</h2>
-            <div class="logPage"><input id="login" name="loginRegister" type="text" placeholder="username"> </div>
-            <div class="logPage"><input id="email" name="emailRegister" type="text" placeholder="email"> </div>
-            <div class="logPage"><input id="password" name="passwordRegister" type="password" placeholder="******"></div>
-            <div class="logPage"><input type="submit" value="Register" name="subReg"></div>
-            <div class="logPage">Already registered? <a href=" login.php">Sign In</a></div>
+    <div class="signup">
+        <h2>Register</h2>
+        <form action="app.php" method="post" class = "form_signup">
+            <div class="signup_uname"><input id="login" name="loginRegister" type="text" placeholder="username" class = "text"> </div>
+            <div class="signup_email"><input id="email" name="emailRegister" type="text" placeholder="email" class = "text"> </div>
+            <div class="signup_passwd"><input id="password" name="passwordRegister" type="password" placeholder="*********" class = "text"></div>
+            <div class="signup_button"><input type="submit" value="Register" name="subReg" class = "submit"></div>
+            <div class="signup_login">Already registered? <br><a href=" login.php"><input type="submit" value="Login" class = "submit_small"></a></div>
         </form>
     </div>
 </body>
