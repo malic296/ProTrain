9988ec0db210da7ccf66348407072c8450dfc648
wajfy <96193238+wajfy@users.noreply.github.com>
2023-01-28 17:30:29 +0100

    Add files via upload

--- DBconnection.php
+++ DBconnection.php
@@ -8,10 +8,12 @@ $DBpassword = "ProTrain2022";
 $db = "db_28564_ProTrain";
 */
 //local
+
 $DBservername = "localhost";
 $DBusername = "root";
 $DBpassword = "";
 $db = "apptestdb";
+
 $connection = mysqli_connect($DBservername, $DBusername, $DBpassword, $db);
 
 ?>
